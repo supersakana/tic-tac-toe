4e32b5ab8d7d9d1327164833fa268268a2b917c9
supersakana <zacwilliamson@icloud.com>
2022-02-22 15:24:21 -0600

    added small introduction. Again to be continued... still on OOP chapter of TOP

--- game.rb
+++ game.rb
@@ -1,3 +1,13 @@
-puts "[[1,'A'][1,'B'][1,'C']]"
-puts "[[2,'A'][2,'B'][2,'C']]"
-puts "[[3,'A'][3,'B'][3,'C']]"
+# ruby game.rb
+puts "Hello and welcome to tic-tac-toe. What is your name?"
+player_1 = gets.chomp
+puts "Welcome #{player_1}" 
+
+
+row_a = ['A1','A2','A3']
+row_b = ['B1','B2','B3']
+row_c = ['C1','C2','C3']
+
+p row_a
+p row_b
+p row_c
