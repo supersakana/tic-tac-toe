39314486777c7142f4caa38389faac53cd19c374
supersakana <zacwilliamson@icloud.com>
2022-02-26 08:53:30 -0600

    added player 2 intro

--- game.rb
+++ game.rb
@@ -1,13 +1,20 @@
 # ruby game.rb
-puts "Hello and welcome to tic-tac-toe. What is your name?"
+puts "Hello and welcome to tic-tac-toe!"
+puts "Player 1, what is your name?"
 player_1 = gets.chomp
 puts "Welcome #{player_1}" 
+puts "Player 2, what is your name?"
+player_2 = gets.chomp
+puts "Welcome #{player_2}" 
+
 
 
 row_a = ['A1','A2','A3']
 row_b = ['B1','B2','B3']
 row_c = ['C1','C2','C3']
 
+
 p row_a
 p row_b
 p row_c
+
