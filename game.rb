c2aaf7d7ea109f4908943fe9f4af6a82d0bfa0a0
supersakana <zacwilliamson@icloud.com>
2022-02-27 11:27:49 -0600

    Adding gameboard in a hash, printer prints each array on the screen, variables can be accessed as key value pair... to be continued

--- game.rb
+++ game.rb
@@ -1,36 +1,63 @@
 # ruby game.rb
 class Player
-    attr_accessor :name, :move
-    @@player_count = 0
+  attr_accessor :name, :move
 
-    def initialize(name, move)
-        @name = name
-        @move = move
-        if @@player_count > 2
-            puts "2 player game only"
-        else
-            @@player_count += 1
-        end
+  @@player_count = 0
+
+  def initialize(name, move)
+    @name = name
+    @move = move
+    if @@player_count > 2
+      puts '2 player game only'
+    else
+      @@player_count += 1
     end
+  end
+
+  def move_message
+    puts "#{name}, make your move..."
+  end
+
+  def choice(choice)
+    p choice.split('')
+    p choice[0]
+  end
 end
 
-puts "Hello and welcome to tic-tac-toe!"
+puts 'Hello and welcome to tic-tac-toe!'
 
-puts "Player 1, what is your name?"
-player_1 = Player.new(gets.chomp, "X")
-puts "Welcome #{player_1.name}, your move is #{player_1.move}" 
+puts 'Player 1, what is your name?'
+player_1 = Player.new(gets.chomp, 'X')
+puts "Welcome #{player_1.name}, your move is #{player_1.move}"
 
-puts "Player 2, what is your name?"
-player_2 = Player.new(gets.chomp, "O")
+puts 'Player 2, what is your name?'
+player_2 = Player.new(gets.chomp, 'O')
 puts "Welcome #{player_2.name}, your move is #{player_2.move}"
 
-a = ['A1','A2','A3']
-b = ['B1','B2','B3']
-c = ['C1','C2','C3']
+board = {
+  a: %w[A1 A2 A3],
+  b: %w[B1 B2 B3],
+  c: %w[C1 C2 C3]
+}
 
-p a
-p b
-p c
+def printer
+  board.each do |_key, value|
+    p value
+  end
+end
+printer
 
+# A = %w[A1 A2 A3]
+# B = %w[B1 B2 B3]
+# C = %w[C1 C2 C3]
 
+# def printer
+#   p A
+#   p B
+#   p C
+# end
 
+# printer
+
+# player_1.move_message
+# player_1.choice(gets.chomp.upcase)
