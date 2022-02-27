3b3e2f7fa39850cb1e2a8ff56690da1987c0ebdb
supersakana <zacwilliamson@icloud.com>
2022-02-27 09:26:05 -0600

    added player class

--- game.rb
+++ game.rb
@@ -1,20 +1,36 @@
 # ruby game.rb
+class Player
+    attr_accessor :name, :move
+    @@player_count = 0
+
+    def initialize(name, move)
+        @name = name
+        @move = move
+        if @@player_count > 2
+            puts "2 player game only"
+        else
+            @@player_count += 1
+        end
+    end
+end
+
 puts "Hello and welcome to tic-tac-toe!"
+
 puts "Player 1, what is your name?"
-player_1 = gets.chomp
-puts "Welcome #{player_1}" 
+player_1 = Player.new(gets.chomp, "X")
+puts "Welcome #{player_1.name}, your move is #{player_1.move}" 
+
 puts "Player 2, what is your name?"
-player_2 = gets.chomp
-puts "Welcome #{player_2}" 
+player_2 = Player.new(gets.chomp, "O")
+puts "Welcome #{player_2.name}, your move is #{player_2.move}"
+
+a = ['A1','A2','A3']
+b = ['B1','B2','B3']
+c = ['C1','C2','C3']
+
+p a
+p b
+p c
 
 
-
-row_a = ['A1','A2','A3']
-row_b = ['B1','B2','B3']
-row_c = ['C1','C2','C3']
-
-
-p row_a
-p row_b
-p row_c
 
