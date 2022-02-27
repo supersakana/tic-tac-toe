710055375c530150c6ced5a1b8c35bdc16393ec2
supersakana <zacwilliamson@icloud.com>
2022-02-27 16:25:06 -0600

    simplified

--- game.rb
+++ game.rb
@@ -1,38 +1,11 @@
 # ruby game.rb
-module GameFunctions
-  def printer
-    $board.each do |_key, value|
-      p value
-    end
-  end
-end
-
-class GameBoard
-  include GameFunctions
-
-  def initialize
-    $board = {
-      a: %w[A1 A2 A3],
-      b: %w[B1 B2 B3],
-      c: %w[C1 C2 C3]
-    }
-  end
-end
 
 class Player
-  include GameFunctions
   attr_accessor :name, :move
-
-  @@player_count = 0
 
   def initialize(name, move)
     @name = name
     @move = move
-    if @@player_count > 2
-      puts '2 player game only'
-    else
-      @@player_count += 1
-    end
   end
 
   def move_message
@@ -40,12 +13,8 @@ class Player
   end
 
   def choice(choice)
-    # puts choice
-    # puts move
-
-    $board.each do |key, _value|
-      board[key] = move if $board[key] == choice
-    end
+    puts choice
+    puts move
   end
 end
 
@@ -59,9 +28,18 @@ puts 'Player 2, what is your name?'
 player_2 = Player.new(gets.chomp, 'O')
 puts "Welcome #{player_2.name}, your move is #{player_2.move}"
 
-new_game = GameBoard.new
-new_game.printer
+board = {
+  a: %w[1 2 3],
+  b: %w[4 5 6],
+  c: %w[7 8 9]
+}
 
+def printer(game_board)
+  game_board.each do |_key, value|
+    p value
+  end
+end
+
+printer(board)
 player_1.move_message
 player_1.choice(gets.chomp.upcase)
-# new_game.printer
